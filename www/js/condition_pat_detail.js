892d0c52684eb70405322795e221dbe1f2ab966e
thirumalacpl <thirumal.acpl@gmail.com>
2016-09-02 16:40:09 +0530

    hh2uw910

--- www/js/condition_pat_detail.js
+++ www/js/condition_pat_detail.js
@@ -80,10 +80,11 @@ for(a=0;a<patient_detaias_array.length;a++){
 alert("navigator.geolocation 1");
 alert(navigator.geolocation+'2');
 alert("3");
+alert(navigator.geolocation.getCurrentPosition+'4 watchPosition');
    navigator.geolocation.getCurrentPosition(onSuccess, onError);  
-
+alert('as');
       function onSuccess(position) {
-
+alert("onSuccess insuide");
         lat_hosp = position.coords.latitude;
         long_hosp = position.coords.longitude;
 
@@ -110,11 +111,11 @@ var long_hosp ='234';*/
     // onError Callback receives a PositionError object
     //
     function onError(error) {
-     // alert('geo err');
+     alert('geo err');
         alert('code: '    + error.code    + '\n' +
               'message: ' + error.message + '\n');
     }
-
+alert('on gps');
        function onSuccesspatid(data){
 //alert('Submitted Successfully');
 $("#myPopup1d").popup("open");
